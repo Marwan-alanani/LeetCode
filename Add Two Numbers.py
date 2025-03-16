40d9e9a2219aaf8656c265c49a93be0e878dba81
Marwan <marwan-walid1@hotmail.com>
2025-03-16 20:43:22 +0200

    Improved code for add two numbers

--- Add Two Numbers.py
+++ Add Two Numbers.py
@@ -12,8 +12,10 @@ class Solution(object):
         :type l2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
+
         d1 = []
         d2 = []
+
         while l1 != None:
             d1.append(l1.val)
             l1 = l1.next
@@ -30,43 +32,46 @@ class Solution(object):
         idx2 = len(d2) - 1
         idx3 = len(sm) - 1
         carry = 0
+
         while idx1 >= 0 or idx2 >= 0:
+
             if idx1 >= 0 and idx2 >= 0:
                 sm[idx3] = (d1[idx1] + d2[idx2] + carry) % 10
                 carry = (d1[idx1] + d2[idx2] + carry) // 10
+
             elif idx1 >= 0:
                 sm[idx3] = (d1[idx1] + carry) % 10
                 carry = (d1[idx1] + carry) // 10
+
             elif idx2 >= 0:
                 sm[idx3] = (d2[idx2] + carry) % 10
                 carry = (d2[idx2] + carry) // 10
+
             idx1 -= 1
             idx2 -= 1
             idx3 -= 1
 
-        if carry > 0:
-            sm [idx3] = carry
+        sm [idx3] = carry
 
         start = 0;
 
         while start < len(sm) and sm[start] == 0:
             start +=1
-        
+
         if start == len(sm):
             return ListNode(0)
-        
-        head = None
-        current = None
+
         idx = len(sm) - 1
+        head = ListNode(sm[idx])
+        current = head
+        idx-=1
+
         while idx >= start:
-            if head == None:
-                head = ListNode(sm[idx])
-                current = head
-            else:
-                nw = ListNode(sm[idx])
-                current.next = nw
-                current = nw
+            nw = ListNode(sm[idx])
+            current.next = nw
+            current = nw
             idx-=1
+
         return head
 
 
@@ -80,11 +85,11 @@ def main():
         head = head.next
 
 
-def convert(lst):
+def convert(lst : list):
     head = ListNode(lst[0])
     current = head
     for i in range(1 , len(lst)):
-        nw = ListNode(lst[i]) 
+        nw = ListNode(lst[i])
         current.next = nw
         current = nw
 
